21f3d509d9a209f638606caced88e7168d1f836c
Aditya6306 <adityamishrasln63@gmail.com>
2026-06-18 11:17:06 +0530

    Time: 423 ms (14.56%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,6 +2,6 @@
 
 select query_name,
 round(avg((rating/position)), 2) as quality,
-round(sum(rating<3)*100/count(*), 2) as poor_query_percentage
+round(avg(if(rating<3, 1, 0))*100, 2) as poor_query_percentage
 from Queries 
 group by query_name
