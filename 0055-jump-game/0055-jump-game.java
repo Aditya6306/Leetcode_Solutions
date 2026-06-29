b5073cdc9b31fd3393bfbbb8b0dd6d21b1387f5c
Aditya6306 <adityamishrasln63@gmail.com>
2026-06-29 09:45:25 +0530

    Time: 419 ms (5%), Space: 47.9 MB (42.57%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean check(int[] nums, int i, int[] dp){
+        if(i>=nums.length-1) return true;
+        if(nums[i] == 0) return false;
+        if(dp[i] != -1) return dp[i]==1;
+        boolean ans = false;
+        for(int j=1;j<=nums[i];j++){
+            ans =  check(nums, j+i, dp);
+            if(ans) return ans;
+        }
+        dp[i] = (ans) ? 1 : 0;
+        return  ans;
+    }
+    public boolean canJump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return check(nums, 0, dp);
+
+    }
+}
