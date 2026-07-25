74dff1d2104d0aaebd97ac940d04681da49403f5
Aditya6306 <adityamishrasln63@gmail.com>
2026-07-25 16:08:00 +0530

    Time: 6 ms (68.79%), Space: 46.6 MB (11.81%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,29 +1,31 @@
 class Solution {
-    public static boolean helper(HashSet<String> set, int i, String s, int[] dp){
-        if(set.contains(s.substring(i))) return true;
-        if(i==s.length()) return false;
-        if(dp[i] == 1) return true;
-        if(dp[i] == -1) return false;
-
-        for(int j=i;j<s.length();j++){
-            String str = s.substring(i, j+1);
-            // System.out.println(str+" ");
-            if(set.contains(str)){
-                if(helper(set, j+1, s, dp)) {
-                    dp[i] = 1;
-                    return true;
-                }
-            }
-
+    public boolean check(String s, HashSet<String> set, int i, int j, int[][] dp){
+        if(j>=s.length()){
+            String st=s.substring(i, j);
+            if(set.contains(st)) return true;
+            return false;
         }
-        dp[i]=-1;
-        return false;
-    }
-    public boolean wordBreak(String st, List<String> wordDict) {
-        HashSet<String> set = new HashSet<>();
-        int[] dp = new int[st.length()];
-        for(String s : wordDict) set.add(s);
+        if(dp[i][j] != -1) return dp[i][j]==1;
+        String sub = s.substring(i, j);
+        boolean ans=false;
+        if(set.contains(sub)){
+            ans = ans || check(s, set, j, j+1, dp);
+        }
+        ans = ans || check(s, set, i, j+1, dp);
+        dp[i][j] = (ans==true) ? 1:0;
+        return ans;
+        
 
-        return helper(set, 0, st, dp);
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int[][] dp = new int[s.length()][s.length()];
+        for(int[] ele : dp){
+            Arrays.fill(ele, -1);
+        }
+        for(String ele : wordDict){
+            set.add(ele);
+        }
+        return check(s, set, 0, 1, dp);
     }
 }
