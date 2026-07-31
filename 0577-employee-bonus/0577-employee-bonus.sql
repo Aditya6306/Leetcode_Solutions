0663aee70337640c41c61d9dd58e8bd752cbab78
Aditya6306 <adityamishrasln63@gmail.com>
2026-07-31 11:58:11 +0530

    Time: 986 ms (69.55%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-select e.name, b.bonus from Employee e left join Bonus b
-on b.empId = e.empId
-where b.bonus < 1000 
-or b.bonus is null;
-
-
-
-
-
+select e.name, b.bonus
+from Employee e left join Bonus b
+on e.empId = b.empId
+where b.empId is null
+or b.bonus < 1000;
