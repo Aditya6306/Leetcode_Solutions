757693d6ac6a4233f3fe71591c9a47bd7cfc057d
Aditya6306 <adityamishrasln63@gmail.com>
2026-08-09 22:38:32 +0530

    Time: 43 ms (74.57%), Space: 61.1 MB (58.59%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,38 +1,54 @@
 class Solution {
-    public int calc(ArrayList<Long> list, int num){
-        int lo=0, hi = list.size()-1;
-        int pre=-1;
-        int toIns=-1;
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(list.get(mid)<num) {
-                pre=mid;
-                lo=mid+1;
-            }
-            else hi=mid-1;
+    public void merge(int[] arr, int a[], int b[]){
+        int i=0, j=0, k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<=b[j]) arr[k++]=a[i++];
+            else arr[k++] = b[j++];
         }
-        lo=0; hi=list.size()-1;
-        long n=(long)num*2;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(list.get(mid)<=n){
-                toIns=mid;
-                lo=mid+1;
-            }
-            else hi=mid-1;
+        while(i<a.length){
+            arr[k++]=a[i++];
         }
-        list.add(toIns+1, n);
-        return pre+1;
+        while(j<b.length){
+            arr[k++]=b[j++];
+        }
+    }
+    int ct=0;
+    public void count(int[] nums, int l, int r){
+        if(l>=r){
+            return;
+        }
+        int len = r-l+1;
+
+        int mid = l + len/2;
+
+        int[] a = new int[len/2];
+
+        int b[] = new int[len-(len/2)];
+        int k=0;
+        for(int i=l;i<mid;i++) a[k++] = nums[i];
+        k=0;
+        for(int i=mid;i<=r;i++) b[k++] = nums[i];
+
+        count(a, 0, a.length-1);
+        count(b, 0, b.length-1);
+
+        int i=0, j=0;
+
+        while(i<a.length && j<b.length){
+            long n = (long)2*b[j];
+            if(a[i]>n){
+                int le = a.length-i;
+                ct+=le;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        merge(nums, a, b);
     }
     public int reversePairs(int[] nums) {
-        int n = nums.length;
-        ArrayList<Long> list = new ArrayList<>();
-
-        list.add((long)nums[n-1]*2);
-        int ans=0;
-        for(int i=n-2;i>=0;i--){
-            ans += calc(list, nums[i]);            
-        }
-        return ans;
+        count(nums, 0, nums.length-1);
+        return ct;
     }
 }
