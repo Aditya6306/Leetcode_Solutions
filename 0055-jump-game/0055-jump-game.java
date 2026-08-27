6356f27a83f337b11a200311bc8c3b24756fff60
Aditya6306 <adityamishrasln63@gmail.com>
2026-08-27 13:19:34 +0530

    Time: 381 ms (5.03%), Space: 48.2 MB (8.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,8 +4,8 @@ class Solution {
         if(nums[i] == 0) return false;
         if(dp[i] != -1) return dp[i]==1;
         boolean ans = false;
-        for(int j=1;j<=nums[i];j++){
-            ans =  check(nums, j+i, dp);
+        for(int j=i+1;j<=nums[i]+i;j++){
+            ans =  check(nums, j, dp);
             if(ans) return ans;
         }
         dp[i] = (ans) ? 1 : 0;
